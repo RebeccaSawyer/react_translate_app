6b6fb06ded57d6784e4e2f97d4a5a4f7478931cb
Rebecca <rebecca@Tatyanas-MBP.attlocal.net>
2019-02-19 00:29:27 -0600

    5th commit

--- client/components/HistoryComponent.js
+++ client/components/HistoryComponent.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-	const divStyle = {
- 	display: 'inline-block'
-	}
-
+	
 	class HistoryComponent extends Component {
 		constructor() {
 			super();
@@ -17,43 +14,49 @@ import axios from 'axios';
 	
 		}
 
-		componentDidMount () {
-		axios.get("http://localhost:3000/posts")
+		  componentDidMount () {
+		  axios.get("http://localhost:3000/posts")
 			.then(response => {
 				this.setState({posts: response.data})
 				console.log(response.data);
 			});
-		}
+		  }
   	
   		hideTranslation(event) {
-  		//event.preventDefault();
-  		this.state.hideEnglishWord ? this.setState({hideEnglishWord: false}) : this.setState({hideEnglishWord: true})
-  		//this.props.translate(this.state.word);
-  		//do props.map
-  		//Use componentDidMount to send request to the server to grab the data and pass it as props
-  		//Server will pull it out of database and send it back in response
+  	
+  		event.target.children[0].style.visibility = this.state.hideEnglishWord ? 'hidden' : 'visible'
+      this.state.hideEnglishWord ? this.setState({hideEnglishWord: false}) : this.setState({hideEnglishWord: true})	
+    
   		}
 
   		render () {
-  		const style = this.state.hideEnglishWord ? { visibility: 'hidden', display: 'inline-block'} : 
-  												   { visibility: 'visible', display: 'inline-block', 'margin-left': '20px'};
-  		const newPosts = this.state.posts.map(post => {
+ 
+      var divstyle = {
+      height: '30px',
+      display: 'block',
+      marginLeft: 250,
+      marginRight: 250
+      }
+      var styleE = {
+        display: 'inline-block',
+        visibility: 'hidden'
+      }
+      var styleS = {
+      display: 'inline-block'
+      }
+   
 
-  		return <div onClick={this.hideTranslation}>
+  		var newPosts = this.state.posts.map(post => {
+
+  		return <div style={divstyle} onClick={this.hideTranslation}>
   		
-  		<p style={divStyle}>{post.spanishTR}</p><p style={style}>{post.englishTR}</p>
-  		</div>
-  		//return <p onClick={this.hideTranslation} style={style}>{`${post.spanishTR} ${post.englishTR}`}</p>
-  		//return <div><p id="es" style={styleES}>{post.spanishTR + ' '}</p><p id="en" style={styleEN}>{post.englishTR}</p></div>
-  		
+  		<p style={styleS}>{post.spanishTR + ' '}<p style={styleE}>{post.englishTR}</p></p>
+  		</div>	
   		})
-  		return (
-  		<div>
-  		<p>{newPosts}</p>
-		</div>
-		)
-		}
-		}
+
+  		return (<div><p>{newPosts}</p></div>)
+		  }
+		  }
 	
 
 
